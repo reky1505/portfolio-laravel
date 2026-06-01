0d83efd14cc25ced50b5744592bc8583e128f887
reky1505 <2453-mm2122@smk-ipiems.sch.id>
2026-06-01 19:13:07 +0700

    Initial Laravel Portfolio

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Saya</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+
+    <h1>Portfolio Saya</h1>
+
+</body>
+</html>
